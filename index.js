ed6735cbfab597c18cf100e32d6bca422937d2b9
jagins <jagins629@gmail.com>
2019-11-12 17:02:33 -0600

    runnerAllCaps() passed

--- index.js
+++ index.js
@@ -206,8 +206,14 @@ function getFullNames(runners)
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) 
+{
+  const upperCase = runners.map(function(name)
+  {
+    return name.first_name.toUpperCase();
+  });
+
+  return upperCase;
 }
 
 /**
@@ -223,7 +229,8 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
+function getRunnersByTShirtSize(/* CODE HERE */) 
+{
   /* CODE HERE */
 }
 
